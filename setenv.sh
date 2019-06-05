edd99bd0bf1afd15437ef166812a6b4339a73bb8
thuchoang90 <thuc@vlsilab.ee.uec.ac.jp>
2019-06-05 15:34:11 +0900

    Update setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -1,2 +1,3 @@
 export RISCV="/home/aist-riscv/riscv64-elf-tc"
+export PATH="/home/aist-riscv/riscv64-elf-tc/bin/:$PATH"
 export PATH="/home/aist-riscv/projects/tools/Xilinx/Vivado/2016.4/bin/:$PATH"
