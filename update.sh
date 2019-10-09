7cc30d9455db1df0e28f590163a732ed14618b3a
thuchoang90 <thuc@vlsilab.ee.uec.ac.jp>
2019-10-09 18:18:02 +0900

    bump for new submodule bootrom/freedom-u540-c000-bootloader

--- update.sh
+++ update.sh
@@ -3,6 +3,4 @@ cd rocket-chip/
 git -c submodule.riscv-tools.update=none submodule update --init --recursive
 cd ../fpga-shells/
 patch -p1 < ../fpga-shells.patch
-cd ../bootrom/freedom-u540-c000-bootloader/
-patch -p1 < ../../freedom-u540-c000-bootloader.patch
-cd ../../
+cd ../
