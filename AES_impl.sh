56f9a10b2f1fca0d4a378e6e8aab4f4af9478645
mr13441176 <daobaanhvn1991@gmail.com>
2019-12-19 23:01:38 +0900

    Another minor fix to AES_impl.sh

--- AES_impl.sh
+++ AES_impl.sh
@@ -2,4 +2,5 @@ patch bootrom/freedom-u540-c000-bootloader/lib/memcpy.c BA_Patch_files/AES_impl.
 patch bootrom/freedom-u540-c000-bootloader/fsbl/main.c BA_Patch_files/AES_impl_2.patch
 patch bootrom/freedom-u540-c000-bootloader/uart/uart.c BA_Patch_files/AES_impl_3.patch
 patch bootrom/freedom-u540-c000-bootloader/uart/uart.h BA_Patch_files/AES_impl_4.patch
+patch bootrom/freedom-u540-c000-bootloader/Makefile BA_Patch_files/AES_impl_5.patch
 sudo mv aes/ bootrom/freedom-u540-c000-bootloader/lib/aes/ 
