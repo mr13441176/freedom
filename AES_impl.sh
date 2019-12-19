ba56aed2f2cefcde97b7b1deb881c4f3f94656ad
mr13441176 <daobaanhvn1991@gmail.com>
2019-12-20 01:59:43 +0900

    Minor fix patching bootloader makefile

--- AES_impl.sh
+++ AES_impl.sh
@@ -3,4 +3,4 @@ patch bootrom/freedom-u540-c000-bootloader/fsbl/main.c BA_Patch_files/AES_impl_2
 patch bootrom/freedom-u540-c000-bootloader/uart/uart.c BA_Patch_files/AES_impl_3.patch
 patch bootrom/freedom-u540-c000-bootloader/uart/uart.h BA_Patch_files/AES_impl_4.patch
 patch bootrom/freedom-u540-c000-bootloader/Makefile BA_Patch_files/AES_impl_5.patch
-sudo mv aes/ bootrom/freedom-u540-c000-bootloader/lib/aes/ 
+mv aes/ bootrom/freedom-u540-c000-bootloader/lib/aes/ 
