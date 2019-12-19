3dfec9e276047485832b6419cfbf7c54c00ed212
mr13441176 <daobaanhvn1991@gmail.com>
2019-12-19 14:44:20 +0900

    minor fix for keystone-fpga-dfs-dba:378d0be

--- update.sh
+++ update.sh
@@ -5,4 +5,6 @@ cd ../fpga-shells/
 patch -p1 < ../fpga-shells.patch
 cd ../
 patch sifive-blocks/src/main/scala/devices/uart/UART.scala UART_scala.patch
-patch sifive-blocks/src/main/scala/devices/uart/UARTCtrlRegs.scala UARTCtrlRegs_scala.path
+patch sifive-blocks/src/main/scala/devices/uart/UARTCtrlRegs.scala UARTCtrlRegs_scala.patch
+patch bootrom/freedom-u540-c000-bootloader/fsbl/main.c fsblmain.patch
+patch bootrom/freedom-u540-c000-bootloader/sifive/devices/uart.h sifive_devices_uart_h.patch
